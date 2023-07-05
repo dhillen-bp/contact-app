d8f40f418fd88e188b2b507571c281bd289aec1f
dhillenbp179 <dhillen.mutu@gmail.com>
2023-07-05 16:36:34 +0700

    nodejs web server

---
+++ contact-app-web/app.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
+const host = "127.0.0.1";
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error : file not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  const url = req.url;
+
+  switch (url) {
+    case "/about":
+      renderHTML("./about.html", res);
+      break;
+    case "/contact":
+      renderHTML("./contact.html", res);
+      break;
+    default:
+      renderHTML("./index.html", res);
+      break;
+  }
+
+  //   if (url === "/about") {
+  //     renderHTML("./about.html", res);
+  //   } else if (url === "/contact") {
+  //     renderHTML("./contact.html", res);
+  //   } else {
+  //     // res.write("Hello World!");
+  //     renderHTML("./index.html", res);
+  //   }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is listening on port ${port}`);
+});
