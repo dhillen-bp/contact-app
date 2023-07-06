d08f962816746ee71f9fb0813b2125c252c62b75
dhillenbp179 <dhillen.mutu@gmail.com>
2023-07-06 11:29:25 +0700

    express middleware

--- contact-app-web/app.js
+++ contact-app-web/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-// gunakan ejs
 app.set("view engine", "ejs");
+
+// Third party middleware
 app.use(expressLayouts);
+app.use(morgan("dev"));
+
+// Built in middleware
+app.use(express.static("public"));
+
+// Application level middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
 
 app.get("/", (req, res) => {
   // res.sendFile("./index.html", { root: __dirname });
